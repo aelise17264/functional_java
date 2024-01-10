4b136440e9bc00d01e211a0716bf1dd7d976c8ea
Anna <aelise17264@gmail.com>
2024-01-10 10:31:24 -0700

    streams Reduce

---
+++ src/streams/Reduce.java
@@ -0,0 +1,25 @@
+package streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class Reduce {
+    public static void main(String[] args) {
+        Integer [] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> listOfIntegers = new ArrayList<>(Arrays.asList(intArray));
+
+        BinaryOperator<Integer> getSum = (acc, x) -> {
+            Integer result = acc + x;
+            System.out.println("acc " + acc + ", x " + x + " and result " + result);
+            return result;
+        };
+
+        Integer sum = listOfIntegers.stream().reduce(0, getSum);
+
+        System.out.println("final sum " + sum);
+
+    }
+    
+}
